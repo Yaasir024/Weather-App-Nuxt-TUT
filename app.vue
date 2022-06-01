ea276943c82d17a75f84675dae635a89eaeec060
Yaasir024 <yope4ever@gmail.com>
2022-06-01 12:23:42 +0100

    First Commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen relative overflow-hidden">
+    <img/>
+    <div class="absolute w-full h-full top-0 overlay"/>
+    <div class="absolute w-full h-full top-0 p-48">
+      <div>
+        <h1 class="text-7xl text-white">Monaco</h1>
+        <p class="font-extralight text-2xl mt-2 text-white">Wednesay June 1st</p>
+        <img class="w-56 icon"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.overlay {
+  background-color: rgba(0,0,0,0.5);
+}
+</style>
