b906be4283d9936ed282b7363929b4889fcf85fb
Yaasir024 <yope4ever@gmail.com>
2022-06-01 12:58:06 +0100

    /

--- app.vue
+++ app.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="h-screen relative overflow-hidden">
-    <img/>
-    <div class="absolute w-full h-full top-0 overlay"/>
+    {{ data }}
+    {{ error }}
+    <img />
+    <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
-      <div>
-        <h1 class="text-7xl text-white">Monaco</h1>
-        <p class="font-extralight text-2xl mt-2 text-white">Wednesay June 1st</p>
-        <img class="w-56 icon"/>
+      <div class="flex justify-between">
+        <div>
+          <h1 class="text-7xl text-white">{{ city.name }}</h1>
+          <p class="font-extralight text-2xl mt-2 text-white">
+            Wednesay June 1st
+          </p>
+          <img
+            :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`"
+            class="w-56 icon"
+          />
+        </div>
+        <div>
+          <p class="text-9xl text-white font-extralight">
+            {{ city.main.temp }}°
+          </p>
+        </div>
+      </div>
+      <div class="mt-20">
+        <input type="text" class="w-1/2 h-10" placeholder="Search a city..." />
+        <button class="bg-sky-400 w-20 text-white h-10">Search</button>
       </div>
     </div>
   </div>
 </template>
 
+<script setup lang="ts">
+const { data: city, error } = useFetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=Dubai&units=metric&appid=fa847feedd1e1b63e4e28cc46441e268"
+);
+</script>
+
 <style scoped>
 .overlay {
-  background-color: rgba(0,0,0,0.5);
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.icon {
+  margin-left: -2.75rem;
+  margin-top: -2.75rem;
 }
 </style>
